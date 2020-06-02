0bbb63e6c715333c9ae9cc9f7048e8f042f3ca74
milone365 <49542221+milone365@users.noreply.github.com>
2020-06-02 17:54:46 +0900

    Add files via upload

---
+++ Editor/CreateWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemyDesignerWindow : EditorWindow
+{
+    [MenuItem("Window/Enemy Designer")]
+    static void OpenWindow()
+    {
+        //create window instance
+        EnemyDesignerWindow window = (EnemyDesignerWindow)GetWindow(typeof(EnemyDesignerWindow));
+        //set size window size
+        window.minSize = new Vector2(600, 300);
+        //for see the window
+        window.Show();
+    }
+}
